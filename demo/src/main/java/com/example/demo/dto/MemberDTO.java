84e34a0aa65f28e8938d8411dd0c6f294a513113
gannniii <kke08kr@gmail.com>
2025-04-15 17:49:54 +0900

    spring

---
+++ demo/src/main/java/com/example/demo/dto/MemberDTO.java
@@ -0,0 +1,20 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class MemberDTO {
+    private String userid;
+    private String password;
+    private boolean check;
+}
