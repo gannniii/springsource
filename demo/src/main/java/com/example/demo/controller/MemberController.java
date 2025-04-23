76b709a122c3436e9a3c6ce5fcd0c2c21c86018c
gannniii <kke08kr@gmail.com>
2025-04-23 18:04:11 +0900

    spring7

--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.lang.reflect.Member;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j2;
@@ -15,6 +16,7 @@ import com.example.demo.dto.LoginDTO;
 import com.example.demo.dto.MemberDTO;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,13 +34,19 @@ public class MemberController {
     // void : template/member/register.html
 
     @GetMapping("/register")
-    public void getRegister() {
+    public void getRegister(@ModelAttribute("mDto") MemberDTO memberDTO) {
         log.info("회원가입");
     }
 
     @PostMapping("/register")
-    public String postRegister(@ModelAttribute("mDTO") MemberDTO memberDTO, RedirectAttributes rttr) {
+    public String postRegister(@ModelAttribute("mDto") @Valid MemberDTO memberDTO, BindingResult result,
+            RedirectAttributes rttr) {
         log.info("회원가입 요청 {}", memberDTO);
+
+        // 유효성 검사 통과하지 못한다면 원래 입력 페이지로 돌아가기
+        if (result.hasErrors()) {
+            return "/member/register";
+        }
 
         // 로그인 페이지로 이동
         // redirect 방식으로 가면서 값을 보내고 싶다면?
---
+++ todo/src/main/java/com/example/todo/dto/ToDoDTO.java
@@ -0,0 +1,31 @@
+package com.example.todo.dto;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ToDoDTO {
+    private Long id;
+
+    private String content;
+
+    private boolean completed;
+
+    private boolean importanted;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+}
