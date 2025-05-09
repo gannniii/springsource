9d80ea6e598a5c421de252960b3ac31e652a648d
gannniii <kke08kr@gmail.com>
2025-05-09 18:07:53 +0900

    security1

--- board/src/main/resources/static/js/read.js
+++ board/src/main/resources/static/js/read.js
@@ -15,11 +15,19 @@ const formatDate = (str) => {
   );
 };
 
+const replyListElement = document.querySelector(".replyList");
+const replyForm = document.querySelector("#replyForm");
+
 const replyList = () => {
   axios.get(`/replies/board/${bno}`).then((res) => {
     console.log(res.data);
 
     const data = res.data;
+    console.log("댓글 수 ", data.length);
+
+    // 댓글 갯수 수정
+    replyListElement.previousElementSibling.querySelector("span").innerHTML = data.length;
+
     let result = "";
     data.forEach((reply) => {
       result += `<div class="d-flex justify-content-between my-2 border-bottom reply-row" data-rno=${reply.rno}>`;
@@ -32,7 +40,7 @@ const replyList = () => {
       result += `<div><button class="btn btn-outline-success btn-sm">수정</button></div>`;
       result += `</div></div>`;
     });
-    document.querySelector(".replyList").innerHTML = result;
+    replyListElement.innerHTML = result;
   });
 };
 // axios 사용
@@ -63,6 +71,63 @@ document.querySelector(".replyList").addEventListener("click", (e) => {
     });
   } else if (btn.classList.contains("btn-outline-success")) {
     // 수정
+    axios.get(`/replies/${rno}`).then((res) => {
+      console.log(res.data);
+      const data = res.data;
+
+      // replyForm 안에 보여주기
+      replyForm.rno.value = data.rno;
+      replyForm.replyer.value = data.replyer;
+      replyForm.text.value = data.text;
+    });
+  }
+});
+
+// 폼 submit => 수정 / 삽입
+replyForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const form = e.target;
+  const rno = form.rno.value;
+
+  if (form.rno.value) {
+    // 수정
+    axios
+      .put(`/replies/${rno}`, form, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        alert("댓글 수정 완료");
+        // form 기존 내용 지우기
+        replyForm.rno.value = "";
+        replyForm.replyer.value = "";
+        replyForm.text.value = "";
+
+        // 수정 내용 반영
+        replyList();
+      });
+  } else {
+    // 삽입
+    axios
+      .post("/replies/new", form, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        alert(res.data + "댓글 등록");
+
+        // form 기존 내용 지우기
+        replyForm.rno.value = "";
+        replyForm.replyer.value = "";
+        replyForm.text.value = "";
+
+        // 삽입 내용 반영
+        replyList();
+      });
   }
 });
 
---
+++ security/src/main/java/com/example/security/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.example.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/", "/sample/guest").permitAll()
+                .requestMatchers("/sample/member").hasRole("USER")
+                .requestMatchers("/sample/admin").hasRole("ADMIN")
+                .anyRequest().authenticated())
+                // .httpBasic(Customizer.withDefaults());
+                // .formLogin(Customizer.withDefaults()); // 시큐리티가 제공하는 기본 로그인 폼 페이지
+                .formLogin(login -> login.loginPage("/member/login").permitAll());
+        http.logout(logout -> logout
+                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                .logoutSuccessUrl("/"));
+
+        return http.build();
+    }
+
+    @Bean // = new 한 후 스프링 컨테이너가 관리
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // @Bean
+    // UserDetailsService users() {
+    // UserDetails user = User.builder()
+    // .username("user")
+    // .password("{bcrypt}$2a$10$m5QD62m5rRW.vfIEsHn0pO.PFol0lYDfmGqY1IALzgDUSvSoxBi9S")
+    // .roles("USER") // ROLE_권한명
+    // .build();
+
+    // UserDetails admin = User.builder()
+    // .username("admin")
+    // .password("{bcrypt}$2a$10$m5QD62m5rRW.vfIEsHn0pO.PFol0lYDfmGqY1IALzgDUSvSoxBi9S")
+    // .roles("USER", "ADMIN") // ROLE_권한명
+    // .build();
+    // return new InMemoryUserDetailsManager(user, admin);
+    // }
+
+}
